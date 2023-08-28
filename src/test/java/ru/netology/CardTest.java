2761378ab528ff45640d8bf9ffd59bf6a6a9f7e9
mayer72 <mayer7272@mail.ru>
2023-08-28 16:39:41 +0500

    Initial commit

---
+++ src/test/java/ru/netology/CardTest.java
@@ -0,0 +1,35 @@
+package ru.netology;
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardTest {
+    private String makeDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void shouldSuccessFillForm() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Уфа");
+        String bookedDate = makeDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
+        $("[data-test-id='date'] input").sendKeys(bookedDate);
+        $("[data-test-id='name'] input").setValue("Смирнов Вася");
+        $("[data-test-id='phone'] input").setValue("+71234567890");
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + bookedDate));
+    }
+}
