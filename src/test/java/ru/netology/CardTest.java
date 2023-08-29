44642aeab170cdc389c6a0a51b801b8c4cf9eccf
mayer72 <mayer7272@mail.ru>
2023-08-29 10:31:09 +0500

    test

--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -1,16 +1,11 @@
 package ru.netology;
+
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardTest {
@@ -19,7 +14,7 @@ public class CardTest {
     }
 
     @Test
-    public void shouldSuccessFillForm() throws InterruptedException {
+    public void ValidFormCompletion() throws InterruptedException {
         open("http://localhost:9999");
         //Thread.sleep(5000);
         $("[data-test-id='city'] input").setValue("Москва");
